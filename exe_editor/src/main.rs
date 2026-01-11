a22ad3e401b5e700ce77000865e3b7f23cf2e98e
recatek <recatek@hotmail.com>
2026-01-11 17:50:12 -0500

    Much better implementation of dynlib loading and reflect data extraction. Thank you eugineerd!

--- exe_editor/src/main.rs
+++ exe_editor/src/main.rs
@@ -1,20 +1,13 @@
-// Ugly hacks due to the simple dynamic/static split
-#![allow(unused_variables)]
-#![allow(unused_mut)]
-#![allow(unreachable_code)]
-
 use std::fs;
 use std::io::Result as IoResult;
 use std::path::{Path, PathBuf};
 
 use bevy::prelude::*;
+use bevy::reflect::TypeRegistry;
 use bevy_egui::{EguiContexts, EguiPlugin, EguiPrimaryContextPass, egui};
-use toml::Table;
 
-#[cfg(feature = "static")]
-use lib_game::SomeGameComponent;
-#[cfg(feature = "dynamic")]
-use libloading::{self, Library, Symbol};
+use libloading;
+use toml::Table;
 
 const PATH_TO_BEVY_TOML: &str = "bevy/bevy.toml";
 
@@ -26,8 +19,8 @@ struct ProjectData {
 
 #[derive(Resource, Default)]
 struct GameLibrary {
-    #[cfg(feature = "dynamic")]
-    library: Option<Library>,
+    library: Option<libloading::Library>,
+    registry: Option<TypeRegistry>,
 }
 
 fn main() {
@@ -54,12 +47,19 @@ fn setup_camera_system(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
 
+#[allow(unused_mut)]
+#[allow(unused_variables)]
 fn setup_library(mut res: ResMut<GameLibrary>) {
-    #[cfg(feature = "dynamic")]
-    {
-        unsafe {
-            res.library = Some(Library::new("lib_game.dll").expect("failed to load lib_game.dll"));
-        }
+    unsafe {
+        let mut registry = TypeRegistry::empty();
+        let library = libloading::Library::new("lib_game.dll").expect("failed to load game dll");
+        let register = library
+            .get::<fn(&mut TypeRegistry)>(b"do_registration\0")
+            .expect("failed to load symbol -- is the game dylib up to date?");
+        register(&mut registry);
+
+        res.library = Some(library);
+        res.registry = Some(registry);
     }
 }
 
@@ -75,14 +75,23 @@ fn setup_project_data(mut project: ResMut<ProjectData>) {
     walk_files(&root_path, &mut project.files).unwrap();
 }
 
-fn show_reflected_data(mut contexts: EguiContexts, mut library: ResMut<GameLibrary>) -> Result {
-    let field_names = library.get_field_names();
+fn show_reflected_data(
+    mut contexts: EguiContexts,
+    library: ResMut<GameLibrary>,
+    registry: Res<AppTypeRegistry>,
+) -> Result {
+    let game_struct_info = get_game_struct_info(&*library, &*registry);
 
     egui::Window::new("Reflected Data").show(contexts.ctx_mut()?, |ui| {
-        for name in field_names {
-            ui.label(&name);
+        for (struct_name, fields) in game_struct_info {
+            ui.label(struct_name);
+
+            for field in fields {
+                ui.label(format!(" - {}", field));
+            }
         }
     });
+
     Ok(())
 }
 
@@ -122,39 +131,34 @@ fn trim_path(path: &Path) -> PathBuf {
     path.components().skip(1).collect()
 }
 
-impl GameLibrary {
-    fn get_field_names(&mut self) -> Vec<String> {
-        #[cfg(feature = "static")]
-        {
-            let component = SomeGameComponent::default();
-            return if let Some(struct_info) = component.get_represented_struct_info() {
-                struct_info
-                    .field_names()
-                    .iter()
-                    .map(|s| s.to_string())
-                    .collect()
-            } else {
-                vec![]
-            };
+#[allow(unused_mut)]
+#[allow(unused_variables)]
+fn get_game_struct_info(
+    library: &GameLibrary,
+    registry: &AppTypeRegistry,
+) -> Vec<(String, Vec<String>)> {
+    let mut result = Vec::new();
+    let registry_lock = registry.internal.read().unwrap();
+    let mut registry = &*registry_lock;
 
-            unreachable!();
-        }
-
-        #[cfg(feature = "dynamic")]
-        {
-            let library = self.library.as_mut().expect("missing library");
-
-            unsafe {
-                let symbol = library
-                    .get::<*mut &[&str]>(b"__REFLECT_FIELD_NAMES_SomeGameComponent\0")
-                    .expect("Failed to load symbol -- is the DLL up to date?");
-
-                return (**symbol).iter().map(|s| s.to_string()).collect();
-            }
-
-            unreachable!()
-        }
-
-        unreachable!()
+    if let Some(library_registry) = library.registry.as_ref() {
+        registry = library_registry;
     }
+
+    for data in registry.iter() {
+        let type_info = data.type_info();
+        if type_info.type_path().starts_with("lib_game") {
+            if let Ok(struct_info) = type_info.as_struct() {
+                let mut fields = Vec::new();
+
+                for field in struct_info.iter() {
+                    fields.push(field.name().to_string());
+                }
+
+                result.push((type_info.type_path().to_string(), fields));
+            }
+        }
+    }
+
+    result
 }
--- lib_game/src/lib.rs
+++ lib_game/src/lib.rs
@@ -1,6 +1,11 @@
 use bevy::prelude::*;
+use bevy::reflect::TypeRegistry;
 
 pub struct GamePlugin;
+
+pub struct EditorRegistrar(pub fn(&mut TypeRegistry));
+
+inventory::collect!(EditorRegistrar);
 
 #[derive(Component, Default, Reflect)]
 pub struct SomeGameComponent {
@@ -8,11 +13,32 @@ pub struct SomeGameComponent {
     pub value: f32,
 }
 
-// This would be macro-generated?
-#[allow(non_upper_case_globals)]
-pub static __REFLECT_FIELD_NAMES_SomeGameComponent: &[&str] = &["name", "value"];
-#[allow(non_upper_case_globals)]
-pub static __REFLECT_FIELD_TYPES_SomeGameComponent: &[&str] = &["String", "f32"];
+// This would be macro-generated
+impl SomeGameComponent {
+    pub fn register_with(registry: &mut TypeRegistry) {
+        registry.register::<Self>();
+    }
+}
+inventory::submit! {
+    EditorRegistrar(SomeGameComponent::register_with)
+}
+
+#[derive(Component, Default, Reflect)]
+pub struct SomeOtherGameComponent {
+    pub is_good: bool,
+    pub width: u32,
+    pub height: u32,
+}
+
+// This would be macro-generated
+impl SomeOtherGameComponent {
+    pub fn register_with(registry: &mut TypeRegistry) {
+        registry.register::<Self>();
+    }
+}
+inventory::submit! {
+    EditorRegistrar(SomeOtherGameComponent::register_with)
+}
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
@@ -23,4 +49,11 @@ impl Plugin for GamePlugin {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
     commands.spawn(Sprite::from_image(asset_server.load("sprites/icon.png")));
+}
+
+#[unsafe(no_mangle)]
+pub fn do_registration(registry: &mut TypeRegistry) {
+    for registrar in inventory::iter::<EditorRegistrar>() {
+        registrar.0(registry)
+    }
 }
